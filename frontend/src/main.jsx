fdc11d067d3e484b1d886bc6af37d86e2f9592e4
misriyaa <misriya8784@gmail.com>
2026-05-17 20:13:56 +0530

    force rebuild

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { AppContextProvider } from "./context/AppContent.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
-
+// force rebuild
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
